df1af286c37acfea5c2d95c2e62039d46709b335
George Platon <george.platon@buddyguard.io>
2018-03-02 22:44:15 +0100

    Initial project structure

---
+++ src/main/java/FleetResource.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * This is the main Web REST Api endpoint which is gonna serve the requests to the service.
+ *
+ * Created with ♥ by georgeplaton on 02.03.18.
+ */
+public class FleetResource {
+
+}
---
+++ src/main/java/FleetService.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * This is the main service which does all fleet related logic.
+ *
+ * Created with ♥ by georgeplaton on 02.03.18.
+ */
+public class FleetService {
+
+
+
+    public int calculateFleetEngineers() {
+
+
+        // To be implemented.
+        return 0;
+    }
+
+}
